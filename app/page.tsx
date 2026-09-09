e85b3a7387ae6072c4025ef0da2e274f3e38160b
Abdul Haseeb <haseebclubi33@gmail.com>
2026-09-09 18:36:40 +0500

    Add Vercel Analytics

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Hero from "../src/components/Hero";
 import Features from "../src/components/Features";
 import CTA from "../src/components/CTA";
 import Footer from "../src/components/Footer";
+import { Analytics } from "@vercel/analytics/next"
 
 export default function Home() {
   return (
